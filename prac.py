1f3aeb1700a4f19f4bd7ef8b6bb562874e25a65b
MJephat <mukundijephat22@gmail.com>
2023-05-30 12:05:32 +0300

    create, inserted and read

---
+++ prac.py
@@ -0,0 +1,59 @@
+#  Using a use-case of your choice, apply the concepts of ORM and SQLAlchemy to demonstrate these functionalities ~ Create, Read, Update, and Delete with SQLAlchemy. Due: 12pm
+
+#!/usr/bin/env python3
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+Base = declarative_base()
+
+class City(Base):
+    __tablename__ = 'cities'
+
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
+    population = Column(Integer())
+    governor = Column(String())
+
+if __name__ == '__main__':
+    engine = create_engine('sqlite:///cities.db')
+    Base.metadata.create_all(engine)
+
+     # use our engine to configure a 'Session' class
+    Session = sessionmaker(bind=engine)
+    # use 'Session' class to create 'session' object
+    session = Session()
+
+    city1 = City(
+        name ="Nairobi City",
+        population = "57463636",
+        governor = "Hon. Sakaja"
+    )
+    city2 = City(
+        name ="Mombasa City",
+        population = "47465636",
+        governor = "Hon. Abdi"
+    )
+    city3 = City(
+        name ="Kisumu City",
+        population = "64789389",
+        governor = "Hon. Anyang' Ny'ong'o"
+    )
+    city4 = City(
+        name ="Nakuru City",
+        population = "36770830",
+        governor = "Hon. Lee Kinyanjui"
+    )
+    
+
+    session.add_all([city1,city2,city3,city4])
+    session.commit()
+
+    session.bulk_save_objects([city1, city1, city2, city3, city4])
+    session.commit()
+
+    cities = session.query(City).all()
+    for city in cities:
+       print(city.governor)
